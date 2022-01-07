10a5565e3f053cf9921ba636a8896c7e23a0ad08
nicong <liangningcong@gmail.com>
2022-01-07 17:09:34 +0800

    update

--- pages/systemjs.tsx
+++ pages/systemjs.tsx
@@ -1,22 +1,33 @@
 import Script from 'next/script';
 import React, { useEffect, useState } from 'react';
 
-const config = {
+// 配置模块来源
+let commonImportMap = {
   "imports": {
     "classnames": "https://ga.system.jspm.io/npm:classnames@2.3.1/index.js",
+    "object-assign": "https://ga.system.jspm.io/npm:object-assign@4.1.1/index.js",
+    "react": "https://ga.system.jspm.io/npm:react@17.0.2/index.js"
+  },
+  "scopes": {}
+}
+const qrcodeImportMap = {
+  "imports": {
     "qrcode.react": "https://ga.system.jspm.io/npm:qrcode.react@1.0.1/lib/index.js",
-    "IButton": "./bricks/button/index.sy.js"
   },
   "scopes": {
     "https://ga.system.jspm.io/": {
-      "object-assign": "https://ga.system.jspm.io/npm:object-assign@4.1.1/index.js",
       "prop-types": "https://ga.system.jspm.io/npm:prop-types@15.8.0/index.js",
       "qr.js/lib/ErrorCorrectLevel": "https://ga.system.jspm.io/npm:qr.js@0.0.0/lib/ErrorCorrectLevel.js",
-      "qr.js/lib/QRCode": "https://ga.system.jspm.io/npm:qr.js@0.0.0/lib/QRCode.js",
-      "react": "https://ga.system.jspm.io/npm:react@17.0.2/index.js"
-    },
+      "qr.js/lib/QRCode": "https://ga.system.jspm.io/npm:qr.js@0.0.0/lib/QRCode.js"
+    }
+  }
+}
+const buttonImportMap = {
+  "imports": {
+    "IButton": "./bricks/button/index.sy.js"
+  },
+  "scopes": {
     "./bricks/": {
-      "react": "https://ga.system.jspm.io/npm:react@17.0.2/index.js"
     }
   }
 }
@@ -27,10 +38,19 @@ interface CompType {
   id: string
 }
 
+function sleep(timeout = 0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  })
+}
+
 export default function Systemjs() {
   const [components, setComponents] = useState<CompType[]>([])
 
 	async function loadQrcode() {
+    importMap(qrcodeImportMap)
+    await window.System.prepareImport(1)
+
     const module = await window.System.import('qrcode.react');
 
     setComponents([
@@ -44,6 +64,9 @@ export default function Systemjs() {
 	}
 
   async function loadBtn() {
+    importMap(buttonImportMap)
+    await window.System.prepareImport(1)
+
     const module = await window.System.import('IButton');
 
     setComponents([
@@ -56,7 +79,7 @@ export default function Systemjs() {
     ])
   }
 
-  function importMap() {
+  function importMap(config: object) {
     const el = document.createElement('script');
     el.type = "systemjs-importmap"
     el.innerText = JSON.stringify(config);
@@ -65,7 +88,7 @@ export default function Systemjs() {
 
   useEffect(() => {
     if (process.browser) {
-      importMap()
+      importMap(commonImportMap)
     }
   }, [])
 
